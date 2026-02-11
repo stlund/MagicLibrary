1a598fa11593a32a591d06291a3e831c705370dd
Thomas Östlund <thomas.ostlund@gmail.com>
2026-02-11 13:49:45 +0100

    Simple bookreader proj done.

--- MagicLibrary/Program.cs
+++ MagicLibrary/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using MagicLibrary;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        //MAIN
+        Book b1 = new Book {Title = "The Great Gatsby", Pages = 180 };
+        Book b2 = new Book {Title = "To Kill a Mockingbird", Pages = 281 };
+
+        Reader r1 = new Reader { Name = "Alice", Energy = 100 };
+        Reader r2 = new Reader { Name = "Bob", Energy = 20 };
+
+        int usedEnergy = r1.ReadBook(b1);
+
+        Console.WriteLine($"{r1.Name} använde {usedEnergy} energi. Energy kvar: {r1.Energy}");
+
+        usedEnergy = r2.ReadBook(b2);
+        Console.WriteLine($"{r2.Name} använde {usedEnergy} energi. Energy kvar: {r2.Energy}");
+
+        // Visa totala antal lästa böcker
+        Console.WriteLine($"Totalt lästa böcker: {LibraryService.totalBookRead}");
+        Console.WriteLine();
     }
 }
--- MagicLibrary/Reader.cs
+++ MagicLibrary/Reader.cs
@@ -6,12 +6,27 @@ namespace MagicLibrary
 {
     internal class Reader
     {
-        public string Name { get; set; }
-        public int Energy { get; set; } = 100;
+        public string Name { get; set; } = "Unknown Reader";
+        public int Energy { get; set; } = 200;
 
-        public void ReadBook(Book book)
+        public int ReadBook(Book book)
         {
-            Energy -= book.Pages / (book.Pages / 2);
+            // Condition to check if the reader has enough energy to read the book
+            int requiredEnergy = book.Pages / 2;
+            if (Energy < requiredEnergy)
+            {
+                Console.WriteLine($"{Name} has not enough energy to read the book {book.Title}");
+                return 0;
+            }
+
+            else
+            {
+                Console.WriteLine($"{Name} has enough energy to read the book {book.Title}");
+                int usedEnergy = book.Pages / 2;
+                Energy -= usedEnergy;
+                LibraryService.LogRead();
+                return usedEnergy;
+            }
         }
     }
 }
